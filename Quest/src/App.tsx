8cdcdb40386f04e08978f8f7ca31b7fba10d3ae8
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-04-01 15:25:59 +0200

    Initial commit

--- Quest/src/App.tsx
+++ Quest/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -13,9 +14,19 @@ const pokemonList = [
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <button type="button" onClick={() => setPokemonName("bulbasaur")}> Bulbasaur </button>
+      <button type="button" onClick={() => setPokemonName("mew")}> Mew </button>
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
